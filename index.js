97c912f70b98c06b47ff79096a3c5adc2f0028c8
geoyws <geoyws@gmail.com>
2015-02-13 07:16:36 +0800

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname + '/public'));
+app.post('/', function(req, res) { res.render('public/index.html'); });
 
 var port = process.env.PORT || 8000;
 app.listen(port, function() { console.log('Listening at: ' + port)});
