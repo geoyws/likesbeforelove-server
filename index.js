05b4e9cdc6272a56166b208f684237c1f0cb29e3
geoyws <geoyws@gmail.com>
2015-02-14 19:57:37 +0800

    asdf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname + '/public'));
-app.post('/', function(req, res) { res.sendFile(__dirname, '../public', 'index.html'); });
+app.post('/', function(req, res) { res.sendFile(path.join(__dirname, '../public', 'index.html')); });
 
 var port = process.env.PORT || 8000;
 app.listen(port, function() { console.log('Listening at: ' + port)});
