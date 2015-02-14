2bb9416e92b949a69048b28c6c39bacd49b47619
geoyws <geoyws@gmail.com>
2015-02-14 22:21:25 +0800

    random stuff again

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var path = require('path');
+//var path = require('path');
 var app = express();
 
-app.use('/', express.static(__dirname + '/public'));
-app.all('/', express.static(__dirname + '/public'));
+//app.use('/', express.static(__dirname + '/public'));
+app.post('/', function(req, res) { res.sendFile('./public/index.html') });
 //app.all('/', function(req, res) { res.sendFile(path.join(__dirname, '/public', '/index.html')); });
 
 var port = process.env.PORT || 8000;
