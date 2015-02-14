3e4e9fdab7cbced6e3d85ac3b3ca8f06152d34d8
geoyws <geoyws@gmail.com>
2015-02-14 22:07:38 +0800

    fa;slkdfj

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var path = require('path');
 var app = express();
 
 app.use('/', express.static(__dirname + '/public'));
-app.all('/', function(req, res) { res.sendFile(path.join(__dirname, '/public', 'index.html')); });
+app.all('/', express.static(__dirname + '/public'));
+//app.all('/', function(req, res) { res.sendFile(path.join(__dirname, '/public', '/index.html')); });
 
 var port = process.env.PORT || 8000;
 app.listen(port, function() { console.log('Listening at: ' + port)});
