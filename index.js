08e3b089e58d0ad90aa608cc076c1171f6868908
geoyws <geoyws@gmail.com>
2015-02-14 22:52:52 +0800

    hrm

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.use('/', express.static(__dirname + '/public'));
 //app.post('/', function(req, res) { res.sendFile('./public/index.html') });
 //app.post('/', function(req, res) { res.sendFile(path.join(__dirname, '/public', '/index.html')); });
-app.post('/', function(req, res) { res.sendFile(path.join(__dirname, '/public', '/index.html')); });
+app.post('/', function(req, res) { res.sendfile('index.html', { root: './public' }); });
 
 var port = process.env.PORT || 8000;
 app.listen(port, function() { console.log('Listening at: ' + port)});
