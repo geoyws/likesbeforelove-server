701b20d37b4301258719985348dc1aa8768aff3b
geoyws <geoyws@gmail.com>
2015-02-17 11:42:40 +0800

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use('/', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 //app.post('/', function(req, res) { res.sendFile('./public/index.html') });
 //app.post('/', function(req, res) { res.sendFile(path.join(__dirname, '/public', '/index.html')); });
 app.post('/', function(req, res) { res.sendfile('index.html', { root: './public' }); });
